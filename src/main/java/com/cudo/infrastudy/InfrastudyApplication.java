768c4fb1914074e811e2b081243508fc93c2f27a
KOHEEJI <sglee@cudo.co.kr>
2024-03-12 10:07:13 +0900

    Initial commit

---
+++ src/main/java/com/cudo/infrastudy/InfrastudyApplication.java
@@ -0,0 +1,13 @@
+package com.cudo.infrastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfrastudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfrastudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cudo/infrastudy/InfrastudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cudo.infrastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrastudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
